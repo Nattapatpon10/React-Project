dcc94a2ba4a114eb30e2499b6dcefbcd0ae85ec1
Nattapatpon10 <nnatnaja2@gmail.com>
2023-06-28 10:22:16 +0700

    deplot-test

--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-
-    proxy: {
-
-      "/api": {
-
-        target: "https://script.google.com/macros/s/AKfycbz5NLFpEcv7r1KRq_zXPMHRkKRzOaFOo7e2d9R7e2QDCVPRYNxp6t2KEhZJbDQ8oVCajA/exec?action=",
-
-        changeOrigin: true,
-
-        rewrite: (path) => path.replace(/^\/api/,"")
-
-      }
-
-    }
-
-  }
+  base:'/React-Project',
 })
